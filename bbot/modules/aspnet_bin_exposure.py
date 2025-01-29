0f93014cab59a245da00fee26b560e851cc7e7dd
liquidsec <paul.mueller08@gmail.com>
2025-01-28 23:02:39 -0500

    formatting

--- bbot/modules/aspnet_bin_exposure.py
+++ bbot/modules/aspnet_bin_exposure.py
@@ -1,5 +1,6 @@
 from bbot.modules.base import BaseModule
 
+
 class aspnet_bin_exposure(BaseModule):
     watched_events = ["URL"]
     produced_events = ["VULNERABILITY"]
@@ -11,7 +12,13 @@ class aspnet_bin_exposure(BaseModule):
     }
 
     in_scope_only = True
-    test_dlls = ["Telerik.Web.UI.dll", "Newtonsoft.Json.dll", "System.Net.Http.dll", "EntityFramework.dll", "AjaxControlToolkit.dll"]
+    test_dlls = [
+        "Telerik.Web.UI.dll",
+        "Newtonsoft.Json.dll",
+        "System.Net.Http.dll",
+        "EntityFramework.dll",
+        "AjaxControlToolkit.dll",
+    ]
 
     async def setup(self):
         self.scanned_tracker = set()
@@ -22,22 +29,30 @@ class aspnet_bin_exposure(BaseModule):
         return str(url.rstrip("/") + "/").lower()
 
     async def handle_event(self, event):
-
         normalized_url = self.normalize_url(event.data)
         self.scanned_tracker.add(normalized_url)
         for test_dll in self.test_dlls:
-            for technique in ["b/(S(X))in/###DLL_PLACEHOLDER###/(S(X))/","(S(X))/b/(S(X))in/###DLL_PLACEHOLDER###"]:
-
-                test_url = f"{normalized_url}{technique.replace("###DLL_PLACEHOLDER###",test_dll)}"
+            for technique in ["b/(S(X))in/###DLL_PLACEHOLDER###/(S(X))/", "(S(X))/b/(S(X))in/###DLL_PLACEHOLDER###"]:
+                test_url = f"{normalized_url}{technique.replace('###DLL_PLACEHOLDER###', test_dll)}"
                 self.debug(f"Sending test URL: [{test_url}]")
                 kwargs = {"method": "GET", "allow_redirects": False, "timeout": 10}
                 test_result = await self.helpers.request(test_url, **kwargs)
                 if test_result:
-                    if test_result.status_code == 200 and ('content-type' in test_result.headers and 'application/x-msdownload' in test_result.headers['content-type']):
-                        self.debug(f"Got positive result for probe with test url: [{test_url}]. Status Code: [{test_result.status_code}] Content Length: [{len(test_result.content)}]")
+                    if test_result.status_code == 200 and (
+                        "content-type" in test_result.headers
+                        and "application/x-msdownload" in test_result.headers["content-type"]
+                    ):
+                        self.debug(
+                            f"Got positive result for probe with test url: [{test_url}]. Status Code: [{test_result.status_code}] Content Length: [{len(test_result.content)}]"
+                        )
                         description = f"IIS Bin Directory DLL Exposure. Detection Url: [{test_url}]"
                         await self.emit_event(
-                            {"severity": "HIGH", "host": str(event.host), "url": normalized_url, "description": description},
+                            {
+                                "severity": "HIGH",
+                                "host": str(event.host),
+                                "url": normalized_url,
+                                "description": description,
+                            },
                             "VULNERABILITY",
                             event,
                             context="{module} detected IIS Bin Directory DLL Exposure vulnerability",
