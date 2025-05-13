997e0afdee0f3ed4cdc73cefddfabe298dcac8ba
Michael Monsivais <michael.monsivais@gmail.com>
2025-05-12 20:25:27 -0600

    Update dehashed tests to complete code coverage.
    Added tests for:
    - Invalid email addresses.
    - API/HTTP Errors.
    - Number of results exceeds retrieval limit.

--- bbot/test/test_step_2/module_tests/test_module_dehashed.py
+++ bbot/test/test_step_2/module_tests/test_module_dehashed.py
@@ -1,33 +1,5 @@
 from .base import ModuleTestBase
 
-dehashed_domain_response = {
-    "balance": 10000,
-    "entries": [
-        {
-            "id": "4363462346",
-            "email": ["bob@blacklanternsecurity.com"],
-            "ip_address": ["127.0.0.9"],
-            "username": ["bob@bob.com"],
-            "hashed_password": ["$2a$12$pVmwJ7pXEr3mE.DmCCE4fOUDdeadbeefd2KuCy/tq1ZUFyEOH2bve"],
-            "name": ["Bob Smith"],
-            "phone": ["+91283423839"],
-            "database_name": "eatstreet",
-            "raw_record": {"le_only": True, "unstructured": True},
-        },
-        {
-            "id": "234623453454",
-            "email": ["tim@blacklanternsecurity.com"],
-            "username": ["timmy"],
-            "password": ["TimTamSlam69"],
-            "name": "Tim Tam",
-            "phone": ["+123455667"],
-            "database_name": "eatstreet",
-        },
-    ],
-    "took": "61ms",
-    "total": 2,
-}
-
 
 class TestDehashed(ModuleTestBase):
     modules_overrides = ["dehashed", "speculate"]
@@ -40,7 +12,33 @@ class TestDehashed(ModuleTestBase):
         module_test.httpx_mock.add_response(
             url="https://api.dehashed.com/v2/search",
             method="POST",
-            json=dehashed_domain_response,
+            json={
+                "balance": 10000,
+                "entries": [
+                    {
+                        "id": "4363462346",
+                        "email": ["bob@blacklanternsecurity.com"],
+                        "ip_address": ["127.0.0.9"],
+                        "username": ["bob@bob.com"],
+                        "hashed_password": ["$2a$12$pVmwJ7pXEr3mE.DmCCE4fOUDdeadbeefd2KuCy/tq1ZUFyEOH2bve"],
+                        "name": ["Bob Smith"],
+                        "phone": ["+91283423839"],
+                        "database_name": "eatstreet",
+                        "raw_record": {"le_only": True, "unstructured": True},
+                    },
+                    {
+                        "id": "234623453454",
+                        "email": ["tim@blacklanternsecurity.com"],
+                        "username": ["timmy"],
+                        "password": ["TimTamSlam69"],
+                        "name": "Tim Tam",
+                        "phone": ["+123455667"],
+                        "database_name": "eatstreet",
+                    },
+                ],
+                "took": "61ms",
+                "total": 2,
+            },
         )
         await module_test.mock_dns(
             {
@@ -94,3 +92,68 @@ class TestDehashed(ModuleTestBase):
             [e for e in events if e.type == "PASSWORD" and e.data == "tim@blacklanternsecurity.com:TimTamSlam69"]
         )
         assert 1 == len([e for e in events if e.type == "USERNAME" and e.data == "tim@blacklanternsecurity.com:timmy"])
+
+
+class TestDehashedBadEmail(TestDehashed):
+    async def setup_before_prep(self, module_test):
+        module_test.httpx_mock.add_response(
+            url="https://api.dehashed.com/v2/search",
+            method="POST",
+            json={
+                "balance": 10000,
+                "entries": [
+                    {
+                        "id": "EZxg4Lz-INLUt6uRXZaV",
+                        "email": ["foo.example.com"],
+                        "database_name": "Collections",
+                    },
+                ],
+                "took": "41ms",
+                "total": 1,
+            },
+        )
+
+    def check(self, module_test, events):
+        debug_log_content = open(module_test.scan.home / "debug.log").read()
+        assert "Invalid email from dehashed.com: foo.example.com" in debug_log_content
+
+
+class TestDehashedHTTPError(TestDehashed):
+    async def setup_before_prep(self, module_test):
+        module_test.httpx_mock.add_response(
+            url="https://api.dehashed.com/v2/search",
+            method="POST",
+            json={"error": "issue with request body"},
+            status_code=400,
+        )
+
+    def check(self, module_test, events):
+        scan_log_content = open(module_test.scan.home / "scan.log").read()
+        assert (
+            'Error retrieving results from dehashed.com (status code 400): {"error":"issue with request body"}'
+            in scan_log_content
+        )
+
+
+class TestDehashedTooManyResults(TestDehashed):
+    async def setup_before_prep(self, module_test):
+        module_test.httpx_mock.add_response(
+            url="https://api.dehashed.com/v2/search",
+            method="POST",
+            json={
+                "balance": 10000,
+                "entries": [
+                    {
+                        "id": "VXhNxj46SGsW4Lworh-G",
+                        "email": ["bob@bob.com"],
+                        "database_name": "Collections",
+                    },
+                ],
+                "took": "40ms",
+                "total": 10001,
+            },
+        )
+
+    def check(self, module_test, events):
+        scan_log_content = open(module_test.scan.home / "scan.log").read()
+        assert "has 10,001 results in Dehashed. The API can only process the first 10,000 results." in scan_log_content
