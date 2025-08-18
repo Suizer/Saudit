5d89abc6675b138adcfcfe8b70a267fa2e9ce79e
liquidsec <paul.mueller08@gmail.com>
2025-08-18 14:43:23 -0400

    update dnsbimi regex

--- bbot/test/test_step_2/module_tests/test_module_dnsbimi.py
+++ bbot/test/test_step_2/module_tests/test_module_dnsbimi.py
@@ -11,6 +11,7 @@ class TestDnsbimi(ModuleTestBase):
     modules_overrides = ["dnsbimi", "speculate"]
     config_overrides = {
         "modules": {"dnsbimi": {"emit_raw_dns_records": True, "selectors": "default,nondefault"}},
+        "omit_event_types": ["HTTP_RESPONSE", "RAW_TEXT", "DNS_NAME_UNRESOLVED", "FILESYSTEM", "WEB_PARAMETER"],
     }
 
     async def setup_after_prep(self, module_test):
