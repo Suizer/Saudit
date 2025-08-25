5d70c1abc689556d0fb0b043e0209c9416bc4b7d
TheTechromancer <thetechromancer@protonmail.com>
2025-08-25 12:21:27 -0400

    missing ok=true

--- bbot/modules/internal/unarchive.py
+++ bbot/modules/internal/unarchive.py
@@ -62,7 +62,7 @@ class unarchive(BaseInternalModule):
                 context=f'extracted "{path}" to: {output_dir}',
             )
         else:
-            output_dir.rmdir()
+            output_dir.rmdir(missing_ok=True)
 
     async def extract_file(self, path, output_dir):
         extension, mime_type, description, confidence = get_magic_info(path)
